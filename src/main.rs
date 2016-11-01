da9015535f558c6c9553d2ec73c8c37d527d675e
Niklas Claesson <nicke.claesson@gmail.com>
2016-11-01 22:28:44 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate clap;
+
 use std::error::Error;
 use std::io::{self};
 use std::io::prelude::*;
@@ -6,35 +9,73 @@ use std::process::{Command, Stdio};
 use std::thread;
 use std::str;
 
+use clap::{Arg, App, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
-    let mut process = match Command::new("cat")
-                                 .stdin(Stdio::piped())
-                                 .stdout(Stdio::piped())
-                                 .spawn() {
-        Err(why) => panic!("Couldn't spawn cat: {}", why.description()),
-        Ok(process) => process,
-    };
+    let matches = App::new("procServ-ng")
+                          .version("0.1.0")
+                          .author("Niklas Claesson <nicke.claesson@gmail.com>")
+                          .about("Simple process controller")
+                          .arg(Arg::with_name("foreground")
+                               .short("f")
+                               .long("foreground"))
+                          .arg(Arg::with_name("holdoff")
+                               .long("holdoff"))
+                          .arg(Arg::with_name("interactive")
+                               .short("I")
+                               .long("interactive"))
+                          .arg(Arg::with_name("port")
+                               .required(true))
+                          .arg(Arg::with_name("command")
+                               .required(true)
+                               .multiple(true))
+                          .get_matches();
 
-    let stdout = process.stdout.take().unwrap();
-    thread::spawn(move || {
-        let reader = BufReader::new(stdout);
-        for line in reader.lines() {
-            print!("got line: {}\n", line.unwrap())
-        }
-    });
-
-
-    let mut stdin = process.stdin.unwrap();
-    let mut writer = BufWriter::new(&mut stdin);
-    loop {
-        let mut buffer = String::new();
-        io::stdin().read_to_string(&mut buffer);
-        match writer.write_all(buffer.as_bytes()) {
-            Err(why) => panic!("Couldn't write to process: {}",
-                            why.description()),
-            Ok(_) => println!("Sent to process..."),
+    let commands = values_t!(matches, "command", String).unwrap();
+    let mut process;
+    let (command, args) = commands.split_first().unwrap();
+    if args.len() > 0 {
+        process = match Command::new(command)
+                                .args(&args)
+                                .stdin(Stdio::piped())
+                                .stdout(Stdio::piped())
+                                .spawn() {
+            Err(why) => panic!("Couldn't spawn cat: {}", why.description()),
+            Ok(process) => process,
         };
-        writer.flush().unwrap();
+    } else {
+        process = match Command::new(command)
+                                .stdin(Stdio::piped())
+                                .stdout(Stdio::piped())
+                                .spawn() {
+            Err(why) => panic!("Couldn't spawn cat: {}", why.description()),
+            Ok(process) => process,
+        };
+    }
+
+    if matches.is_present("foreground") {
+        let stdout = process.stdout.take().unwrap();
+        thread::spawn(move || {
+            let reader = BufReader::new(stdout);
+            for line in reader.lines() {
+                print!("got line: {}\n", line.unwrap())
+            }
+        });
+    }
+
+
+    if matches.is_present("interactive") {
+        let mut stdin = process.stdin.unwrap();
+        let mut writer = BufWriter::new(&mut stdin);
+        loop {
+            let mut buffer = String::new();
+            io::stdin().read_to_string(&mut buffer);
+            match writer.write_all(buffer.as_bytes()) {
+                Err(why) => panic!("Couldn't write to process: {}",
+                                why.description()),
+                Ok(_) => println!("Sent to process..."),
+            };
+            writer.flush().unwrap();
+        }
     }
 }
