28c2cf548e31b65da360d5df02a0d7956fadf38e
Niklas Claesson <nicke.claesson@gmail.com>
2017-06-09 02:34:55 +0200

    wip

--- src/child.rs
+++ src/child.rs
@@ -13,10 +13,6 @@ use futures::task::{self, Task};
 use pty;
 use tokio_core::reactor::Handle;
 
-//use pty::{TtyServer, FileDesc};
-//use pty::ffi::{WinSize, set_winsize, get_winsize};
-//use mio::deprecated::{PipeReader, PipeWriter};
-
 use history::{History};
 
 #[derive(Debug)]
@@ -38,15 +34,11 @@ pub struct Process {
     history: Rc<RefCell<History>>,
     mailbox: Vec<String>,
     foreground: bool,
-    //ptyserver: TtyServer,
     child: Option<pty::Child>,
-    //cid: Option<u32>,
     exit_status: Option<process::ExitStatus>,
     window_sizes: HashMap<SocketAddr, (pty::Rows, pty::Columns)>,
     stdin: Option<pty::PtySink>,
     stdout: Option<pty::PtyStream>,
-    //stdin: PipeWriter,
-    //stdout: PipeReader,
     handle: Handle,
     pr_task: Option<Task>,
     pw_task: Option<Task>,
@@ -54,21 +46,16 @@ pub struct Process {
 
 impl Process {
     pub fn new(args:Vec<String>, history:Rc<RefCell<History>>, foreground:bool, handle: Handle) -> Process {
-        //pty.register(handle);
         Process {
             args: args,
             history: history,
             mailbox: Vec::new(),
             foreground: foreground,
-            //ptyserver: ptyserver,
             child: None,
-            //cid: None,
             exit_status: None,
             window_sizes: HashMap::new(),
             stdin: None,
             stdout: None,
-            //stdin: stdin,
-            //stdout: stdout,
             handle: handle,
             pr_task: None,
             pw_task: None,
@@ -101,10 +88,6 @@ impl Process {
                 if let Some(task) = self.pw_task.take() {
                     task.notify();
                 }
-                //self.cid = Some(p.id());
-                //self.history.borrow_mut().push(
-                //    HistoryType::Info,
-                //    format!("Successfully spawned {}!\r\n", self.args[0]));
                 println!("Launched {}", self.args[0]);
             }
         };
@@ -149,91 +132,6 @@ impl Process {
     pub fn set_pw_task(&mut self, task: Task) {
         self.pw_task = Some(task);
     }
-
-    //pub fn split(self) -> Result<(ProcessWriters, ProcessReaders), ()> {
-    //    let process = Rc::new(RefCell::new(self));
-    //    Ok((ProcessWriters::new(process.clone()), ProcessReaders::new(process.clone())))
-    //}
-    //pub fn split(self) -> Result<(pty::PipeWriter, pty::PipeReader), ()> {
-    //    match self.child {
-    //        Some(child) => Ok((child.input(), child.output())),
-    //        None => {
-    //            println!("No child...");
-    //            Err(())
-    //        },
-    //    }
-    //}
-
-    //pub fn output(&mut self) -> Option<ProcessReader> {
-    //    Some(ProcessReader::new(self.stdout.as_ref().unwrap().clone()))
-    //}
-
-    //pub fn input(&mut self) -> Option<ProcessWriter> {
-    //    Some(ProcessWriter::new(self.stdin.as_ref().unwrap().clone()))
-    //}
-    //pub fn output(&mut self) -> Option<pty::PtyStream> {
-    //    self.child.as_mut().unwrap().output().take()
-    //}
-
-    //pub fn input(&mut self) -> Option<pty::PtySink> {
-    //    self.child.as_mut().unwrap().input().take()
-    //}
-
-    //pub fn read(&mut self) {
-    //    let mut buffer = [0;2048];
-    //    let len = self.stdout.read(&mut buffer).unwrap();
-    //    let line = match String::from_utf8(buffer[0..len].to_vec()) {
-    //        Err(why) => {
-    //            println!("Failed to parse utf8: {}", why);
-    //            String::new()
-    //        },
-    //        Ok(line) => line,
-    //    };
-    //    //println!("[child stdout]: len {}, {}", line.len(), &line);
-    //    if self.foreground {
-    //        let _ = io::stdout().write_all(&buffer[0..len]);
-    //        let _ = io::stdout().flush();
-    //    }
-    //    self.history.borrow_mut().push(HistoryType::Child, line);
-    //}
-
-    //pub fn stdin(&self) -> &PipeWriter {
-    //    &self.stdin
-    //}
-
-    //pub fn stdout(&self) -> &PipeReader {
-    //    &self.stdout
-    //}
-
-    //pub fn kill(&mut self) {
-    //    if self.is_alive() {
-    //        self.child.as_mut().unwrap().kill().expect("Failed to kill process");
-    //        println!("TODO kill process..");
-    //    }
-    //}
-
-    //pub fn is_alive(&self) -> bool {
-    //    self.child.is_some() && self.exit_status.is_none()
-    //}
-
-    //pub fn send(&mut self, msg:String) {
-    //    self.mailbox.push(msg);
-    //}
-
-    //pub fn write(&mut self) {
-    //    for msg in self.mailbox.drain(..) {
-    //        let _ = self.stdin.write(msg.as_bytes());
-    //    }
-    //}
-
-    //pub fn wait(&mut self) -> &process::ExitStatus {
-    //    if self.exit_status.is_some() {
-    //        return self.exit_status.as_ref().unwrap();
-    //    } else {
-    //        self.exit_status = Some(self.child.as_mut().unwrap().wait().expect("Failed to wait on child"));
-    //    }
-    //    self.exit_status.as_ref().unwrap()
-    //}
 }
 
 pub struct ProcessWriters {
